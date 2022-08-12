5681d2b8f2eb5f43ec5826a40975ee844921128f
Anastasia Yurashchuk <asu45456@gmail.com>
2022-08-12 21:04:54 +0300

    Initial

--- app/src/main/java/com/example/astontraining/MainActivity.kt
+++ app/src/main/java/com/example/astontraining/MainActivity.kt
@@ -2,14 +2,11 @@ package com.example.astontraining
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 
-private const val LOG_TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(LOG_TAG, "Hello World")
     }
 }
