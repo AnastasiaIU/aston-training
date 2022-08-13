4ff616bdc0dd2f75307669e197839ab881f43e06
Anastasia Yurashchuk <asu45456@gmail.com>
2022-08-13 11:02:42 +0300

    Final

--- app/src/main/java/com/example/astontraining/MainActivity.kt
+++ app/src/main/java/com/example/astontraining/MainActivity.kt
@@ -1,12 +1,68 @@
 package com.example.astontraining
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 
+const val EXTRA_MESSAGE = "com.example.astontraining.extra.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
+
+    // References to Views
+    private lateinit var mainButton: Button
+    private lateinit var messageEditText: EditText
+    private lateinit var replyHeadTextView: TextView
+    private lateinit var replyTextView: TextView
+
+    // Result receiver for a started Intent
+    private lateinit var getResult: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Find Views
+        mainButton = findViewById(R.id.button_main)
+        messageEditText = findViewById(R.id.editText_main)
+        replyHeadTextView = findViewById(R.id.text_header_reply)
+        replyTextView = findViewById(R.id.text_message_reply)
+
+        // Set receiver
+        getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                replyTextView.text = it.data?.getStringExtra(EXTRA_REPLY)
+                replyHeadTextView.text = getString(R.string.text_header_reply)
+                replyTextView.visibility = View.VISIBLE
+                replyHeadTextView.visibility = View.VISIBLE
+            }
+        }
+
+        // Set a ClickListener to the button
+        mainButton.setOnClickListener { launchSecondActivity() }
+    }
+
+    /**
+     * Launches the SecondActivity.
+     */
+    private fun launchSecondActivity() {
+
+        // Explicit Intent to launch the SecondActivity
+        val launchIntent = Intent(this, SecondActivity::class.java)
+
+        // Text from the EditText
+        val message = messageEditText.text.toString()
+
+        // Add the text to the Intent as an extra
+        launchIntent.putExtra(EXTRA_MESSAGE, message)
+
+        // Start the SecondActivity
+        getResult.launch(launchIntent)
     }
 }
---
+++ app/src/main/java/com/example/astontraining/SecondActivity.kt
@@ -0,0 +1,52 @@
+package com.example.astontraining
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+const val EXTRA_REPLY = "com.example.astontraining.extra.REPLY"
+
+class SecondActivity : AppCompatActivity() {
+
+    // References to Views
+    private lateinit var secondButton: Button
+    private lateinit var messageTextView: TextView
+    private lateinit var replyEditText: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        // Find Views
+        secondButton = findViewById(R.id.button_second)
+        messageTextView = findViewById(R.id.text_message)
+        replyEditText = findViewById(R.id.editText_second)
+
+        // Set a text from the Intent extra
+        messageTextView.text = intent.getStringExtra(EXTRA_MESSAGE)
+
+        // Set a ClickListener to the button
+        secondButton.setOnClickListener { returnReply() }
+    }
+
+    private fun returnReply() {
+
+        // Intent for the response
+        val replyIntent = Intent()
+
+        // Text from the EditText
+        val reply = replyEditText.text.toString()
+
+        // Add the text to the Intent as an extra
+        replyIntent.putExtra(EXTRA_REPLY, reply)
+
+        // Set the OK result
+        setResult(RESULT_OK, replyIntent)
+
+        // Finish the Activity
+        finish()
+    }
+}
